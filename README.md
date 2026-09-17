f1d00eb51096e1d15e3e8992beef32293225352d
unknown <erikdomnickmaciel@gmail.com>
2026-09-17 00:53:26 -0300

    finalização do projeto, eu acho, e adicionando um README.md

---
+++ README.md
@@ -0,0 +1,7 @@
+este trabalho foi usado parte dele para apresentação do fim da matéria de Web-1 (Desenvolvimento web) do Aprender e Crescer e usado também para um trabalho sobre formulário na matéria de Front-end no curso integrado ao ensino-médio TDS (Técnico em Desenvolvimento de Sistemas).
+
+para você retirar as tags "li"s da tela de Reservas, basta abrir o inspecionar clicando F12, indo até a aba de aplicações ou aplications, depois em armazenamento local ou local storage, depois em file:///, clicando com o botão direito encima da chave ou key "array", e clicando em excluir ou delete.
+
+<img src="imagens/negoney-xique-removebg-preview.png" alt="Mr. Negoney xique">
+
+obrigado por ler
--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const forms = document.getElementById('forms');
 const nome = document.getElementById('nome');
 const email = document.getElementById('email');
-const email = document.getElementById('telef');
-const email = document.getElementById('email');
-const email = document.getElementById('qt');
-const email = document.getElementById('qdd');
-const lista = document.getElementById('qdt');
-const msg = document.getElementById('msgErro');
+const telef = document.getElementById('telef');
+const radioEmail = document.getElementById('radioEmail');
+const qt = document.getElementById('qt');
+const qdd = document.getElementById('qdd');
+const qdt = document.getElementById('qdt');
+const lista = document.getElementById('lista');
+const msgErro = document.getElementById('msgErro');
 const listaRes = document.getElementById('listaRes');
 
 let array = []; //lista la
@@ -17,31 +18,36 @@ const salvar = () => { //funcao la q faz o bagulho de salvar la e seta a lista a
 
 const adicionarNaTela = (conteudo) => { //funcao la q faz o tiricoteco do bagulho pra add na tela, criando um elemento na const iem, 
     const item = document.createElement("li");
-    item.textContent = conteudo;
+    item.innerHTML = conteudo;
     lista.appendChild(item); //coloca algo dentro da lista por exemplo
 }
 
 const some = () => {
     nome.value = "";
-    qteqd.value = "";
+    email.value = "";
+    telef.value = "";
+    qt.value = "";
+    qdd.value = "";
+    qdt.value = "";
 }
 
-msg.style.display = "none";
+msgErro.style.display = "none";
 listaRes.style.display = "none";
 
 forms.addEventListener('submit', (event) => {
     event.preventDefault(); //cncela o evento padrao do navegador quando reinicia a pafgina
 
-    if(nome.value.trim() != "" && qteqd.value.trim() != "") { //trim() tira os espacos e enters na direita e esquerda da string, deixando apenas texto
-        adicionarNaTela(`Nome: ${nome.value.trim()}\nEmail: ${qteqd.value.trim()}\nTelefone: ${nome}\n${nome} pessoas vão no dia ${nome}\nPreferência de contato: ${nome}`); // \n quebra linha la nos baguho la
-        array.push({nome: `${nome.value.trim()}`, qteqd: `${qteqd.value.trim()}`}); //coloca um indice a mais no final da lista e coloca o parametro como esse indice
+    if(nome.value.trim() != "" && email.value.trim() != "" && telef.value.trim() != "" && qt.value.trim() != "" && qdd.value.trim() != "" && qdt.value.trim() != "") { //trim() tira os espacos e enters na direita e esquerda da string, deixando apenas texto
+        const pref = radioEmail === true ? "Email" : "Telefone";
+        adicionarNaTela(`<strong>Número da reserva: ${array.length + 1}</strong><br><strong>Nome:</strong> ${nome.value.trim()}<br><strong>Email:</strong> ${email.value.trim()}<br><strong>Telefone:</strong> ${telef.value.trim()}<br><strong>${qt.value.trim()} pessoas</strong> vão no dia <strong>${qdd.value.trim()} as ${qdt.value.trim()}</strong><br><strong>Preferência de contato:</strong> ${pref}`);
+        array.push({nome: nome.value.trim(), email: email.value.trim(), telef: telef.value.trim(), qt: qt.value.trim(), qdd: qdd.value.trim(), qdt: qdt.value.trim(), pref: pref}); //coloca um indice a mais no final da lista e coloca o parametro como esse indice
         salvar();
         some();
-        msg.style.display = "none";
+        msgErro.style.display = "none";
         listaRes.style.display = "block";
     } else {
         some();
-        msg.style.display = "block";
+        msgErro.style.display = "block";
     }
 });
 
@@ -50,7 +56,7 @@ const carregar = () => {
     if (salvo !== null) {
         array = JSON.parse(salvo); //transforma string la pra bagulho sem string coisado la
         listaRes.style.display = "block";
-        array.map((i) => {adicionarNaTela(i)})
+        array.map((i, index) => {adicionarNaTela(`<strong>Número da reserva: ${index + 1}</strong><br><strong>Nome:</strong> ${i.nome}<br><strong>Email:</strong> ${i.email}<br><strong>Telefone:</strong> ${i.telef}<br><strong>${i.qt} pessoas</strong> vão no dia <strong>${i.qdd} as ${i.qdt}</strong><br><strong>Preferência de contato:</strong> ${i.pref}`)}); //map faz o mesmo negocio la do for do bagulho la mas é melhor e mais facil de entender na minha opiniao
     }
 }
 
